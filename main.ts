1e7a49fe1bee9c849b7f8084e3f69b76e7dd6fba
Piotr <pb394091@students.mimuw.edu.pl>
2023-08-30 23:15:31 +0200

    First release

--- main.ts
+++ main.ts
@@ -1,104 +1,119 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin } from 'obsidian';
+import chartXkcd from 'chart.xkcd';
 
-// Remember to rename these classes and interfaces!
+const EMPTY_HISTORY: SizeHistory = {
+	datapoints: [],
+}
+const MS_IN_MINUTE: number = 60 * 1000;
+const MS_IN_DAY: number = 24 * 60 * MS_IN_MINUTE;
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Datapoint {
+	day: string,
+	size: number
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface SizeHistory {
+	datapoints: Datapoint[]
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SizeHistoryPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		this.app.workspace.onLayoutReady(async () => {
+			await this.updateHistory();
+			this.registerInterval(window.setInterval(async () => await this.updateHistory(), MS_IN_MINUTE));
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addRibbonIcon('line-chart', 'Vault size history', async (evt: MouseEvent) => {
+			const history = await this.updateHistory();
+			new SizeHistoryModal(this.app, history).open();
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async updateHistory(): Promise<SizeHistory> {
+		const history = await this.getHistory();
+		const latestTimestamp = this.getLatestTimeMs(history);
+		const allFiles = this.app.vault.getFiles();
+		const unregisterdFiles = allFiles.filter(value => value.stat.ctime > latestTimestamp + MS_IN_DAY);
+		unregisterdFiles.sort((a, b) => (a.stat.ctime - b.stat.ctime));
+		for (const file of unregisterdFiles) {
+			const creationTime = file.stat.ctime;
+			const date = new Date(creationTime);
+			const dp: Datapoint = {
+				day: date.toISOString().substring(0, 10),
+				size: 1
+			};
+			const last = history.datapoints.last();
+			if (!last) {
+				history.datapoints.push(dp);
+				continue;
+			}
 
+			if (last.day == dp.day) {
+				last.size++;
+			} else {
+				dp.size = last.size + 1;
+				history.datapoints.push(dp);
+			}
+		}
+		const currentDatapoint = {
+			day: new Date().toISOString().substring(0, 10),
+			size: allFiles.length
+		};
+
+		const last = history.datapoints.last();
+		if (!last) {
+			history.datapoints.push(currentDatapoint);
+			return history;
+		}
+
+		if (last.day == currentDatapoint.day) {
+			last.size = allFiles.length;
+		} else {
+			history.datapoints.push(currentDatapoint);
+		}
+
+		await this.saveData(history);
+
+		return history;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async getHistory(): Promise<SizeHistory> {
+		return Object.assign({}, EMPTY_HISTORY, await this.loadData());
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	getLatestTimeMs(history: SizeHistory): number {
+		const lastEnpoint = history.datapoints.last();
+		if (!lastEnpoint) {
+			return -1;
+		}
+		const date = new Date(lastEnpoint.day);
+		return date.getTime();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class SizeHistoryModal extends Modal {
+	history: SizeHistory;
+
+	constructor(app: App, history: SizeHistory) {
 		super(app);
+		this.history = history;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const {modalEl, contentEl, titleEl} = this;
+		titleEl.setText("Vault size history")
+		modalEl.addClass("size-history-modal");
+		const div = contentEl.createEl("div", { cls: "size-history-chart"});
+		const svg = contentEl.createSvg("svg", { cls: "line-chart"})
+		div.appendChild(svg);
+		contentEl.appendChild(div);
+
+		const svgLine = document.querySelector<SVGElement>('.line-chart');
+		if (!svgLine) {
+			return;
+		}
+		new SizeHistoryChart(svgLine, this.history.datapoints, this.app.vault.getName());
 	}
 
 	onClose() {
@@ -107,28 +122,33 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SizeHistoryChart {
+	svgElem: SVGElement;
+	datapoints: Datapoint[];
+	vaultName: string
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	constructor(svgElem: SVGElement, datapoints: Datapoint[], vaultName: string) {
+		const data = []
+		for (const value of datapoints) {
+			data.push({x: value.day, y: value.size});
+		}
+		new chartXkcd.XY(svgElem, {
+			title: 'Vault size',
+			xLabel: 'Date',
+			yLabel: 'Number of files',
+			data: {
+				datasets: [{
+					label: vaultName,
+					data: data,
+				}],
+			},
+			options: {
+				xTickCount: 10,
+				yTickCount: 10,
+				dotSize: 0.5,
+				showLine: true,
+				timeFormat: 'MM-DD-YYYY',
+			},
+		});
 	}
 }
